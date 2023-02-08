dea8cca2ac24d69e148be017f4d764af6d9189de
HMMK99 <hatemkamal1999@gmail.com>
2023-02-08 02:14:19 +0200

    add the header

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import React from 'react';
+// import './App.css';
+import Header from './header';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>building firs Mern App</h1>
+      <Header />
     </div>
   );
 }
